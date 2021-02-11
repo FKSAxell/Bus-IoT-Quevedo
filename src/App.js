a7c431e4e938d472a9393b19fb81d72dfcc9ce14
Axell Concha Regatto <aconcha@ecuaquimica.com.ec>
2021-02-11 14:47:10 -0500

    ant design | base layout | maps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import logo from './img/logo_uteq.png';
+import GoogleMapReact from 'google-map-react';
+import './App.css';
+const { Sider } = Layout;
+const props = {
+  center: {
+    lat: 59.95,
+    lng: 30.33
+  },
+  zoom: 11
+};
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Sider className="menu"  breakpoint="lg" collapsedWidth="0" width="300">
+          <div className="logo" >
+            <img src={logo} alt="Logo" width="40%" height="auto" />
+          </div>
+          <h1 className="titulos">PROYECTO INTEGRADOR</h1>
+        </Sider>
+        <Layout>
+          <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBjtK_qMODLxmV-64vddXB0zX5b0o7MLHg'}}
+          defaultCenter={props.center}
+          defaultZoom={props.zoom}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+         
+        </GoogleMapReact>
+      </div>
+
+        </Layout>
+      </Layout>
+    );
+
+  }
 }
 
 export default App;
+//AIzaSyBjtK_qMODLxmV-64vddXB0zX5b0o7MLHg
