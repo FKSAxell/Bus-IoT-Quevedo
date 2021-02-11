7a45743ec456b3552b21b43d7d22cbb3c58e464a
Axell Concha Regatto <aconcha@ecuaquimica.com.ec>
2021-02-11 17:02:24 -0500

    se coloca marker de testeo

--- src/App.js
+++ src/App.js
@@ -8,13 +8,42 @@ import './App.css';
 const { Sider } = Layout;
 const props = {
   center: {
-    lat: 59.95,
-    lng: 30.33
+    lat: -2.1552256,
+    lng: -79.8948806
   },
-  zoom: 11
+  zoom: 15
 };
 
+const renderMarkers = (map, maps) => {
+  let marker = new maps.Marker({
+  position: { lat: -2.1552256, lng: -79.8948806 },
+  map,
+  title: 'Hello World!',
+  icon:{
+    url: "https://image.flaticon.com/icons/png/512/171/171255.png",
+    scaledSize: new maps.Size(42, 42), // size
+  },
+  animation: maps.Animation.DROP
+});
+
+//animacion
+marker.addListener("click", () =>{
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(maps.Animation.BOUNCE);
+  }
+});
+  return marker;
+ };
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+  
   componentDidMount(){
     console.log("hola")
     db.ref('FirebaseIOT').on('value', (snapshot) => {
@@ -40,7 +69,9 @@ class App extends Component {
           bootstrapURLKeys={{ key: 'AIzaSyBjtK_qMODLxmV-64vddXB0zX5b0o7MLHg'}}
           defaultCenter={props.center}
           defaultZoom={props.zoom}
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
         >
+           
          
         </GoogleMapReact>
       </div>
@@ -54,3 +85,4 @@ class App extends Component {
 
 export default App;
 //AIzaSyBjtK_qMODLxmV-64vddXB0zX5b0o7MLHg
+//AIzaSyBRcgW6vPEOiSoqq18ZBgL_AXSfxArqIHU
